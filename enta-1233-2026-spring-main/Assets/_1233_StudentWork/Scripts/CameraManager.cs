9bdc3d538800290ed0c5b547b8159ad4f31b1e34
YiliW01 <yiliwangqq@gmail.com>
2026-04-16 22:55:04 -0700

    version 1_1_1

--- enta-1233-2026-spring-main/Assets/_1233_StudentWork/Scripts/CameraManager.cs
+++ enta-1233-2026-spring-main/Assets/_1233_StudentWork/Scripts/CameraManager.cs
@@ -16,27 +16,15 @@ public class CameraManager : Singleton<CameraManager>
 
     public void ChangeRoomCamera()
     {
-        _cam[0].Priority = 10;
-        _cam[1].Priority = 10;
-        _cam[2].Priority = 10;
-
-        if (_camRoom[0].inRoom == true)
+        for (int i = 0; i < _cam.Length; i++)
         {
-            _cam[0].Priority = 11;
-            _activeCamera = _cam[0];
-        }
+            _cam[i].Priority = 10;
 
-        if (_camRoom[1].inRoom == true)
-        {
-            _cam[1].Priority = 11;
-            _activeCamera = _cam[1];
+            if (_camRoom[i].inRoom == true)
+            {
+                _cam[i].Priority = 11;
+                _activeCamera = _cam[i];
+            }
         }
-
-        if (_camRoom[2].inRoom == true)
-        {
-            _cam[2].Priority = 11;
-            _activeCamera = _cam[2];
-        }
-
     }
 }
